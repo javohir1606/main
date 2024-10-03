156b6a3687a5d26a47d7de4a8dea281dd64199a0
javohir1606 <javohirallayorov1606@gmail.com>
2024-10-03 17:45:16 +0500

    main

--- main/src/Data/main.js
+++ main/src/Data/main.js
@@ -1,3 +1,14 @@
+export const banners = async () => {
+  try {
+    const res = fetch('http://localhost:3600/banners');
+    const data = (await res).json();
+    return data;
+  } catch (error) {
+    return error.massage
+  }
+}
+
+
 export const catalog = async () => {
   try {
     const res = await fetch('http://localhost:3600/catalog');
@@ -19,3 +30,15 @@ export const catalogData = async (name) => {
     return error.massage;
   }
 }
+
+export const catalogBrand = async (brand) => {
+  try {
+    const res = await fetch(`http://localhost:3600/${brand}`);
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    return error.massage;
+  }
+}
+
--- main/src/Data/script.js
+++ main/src/Data/script.js
@@ -1,14 +1,26 @@
+const hero = document.querySelector(".hero");
 const menu = document.querySelector(".menu");
-const box = document.querySelector(".box")
+const box = document.querySelector(".box");
+const row = document.querySelector(".row");
 
-import { catalog, catalogData } from "./main.js";
+import { banners, catalog, catalogData, catalogBrand} from "./main.js";
 
+const renderGet = async () => {
+  const data = await banners();
+  hero.innerHTML = data.map((item) => `
+  <div class="pt-10">
+  <img src="${item.img}" alt="">
+  </div>
+  `).join("");
+}
+
+renderGet()
 
 const render = async () => {
   const data = await catalog();
 
   menu.innerHTML = data.map((item) => `
-  <div class="border border-spacing-x-96 bg-slate-800 p-3">
+  <div class="border rounded-[15px] bg-slate-800 p-3">
    <img src="${item.img}">
    <h2 class="font-bold text-lg text-green-700">${item.text}</h2>
    <button class="border border-spacing-10 py-1 px-2 font-medium text-lg text-red-400 rounded-[10px]" data-name="${item.name}">${item.name}</button>
@@ -19,16 +31,17 @@ render();
 
 const renderData = async (name) => {
     const data = await catalogData(name)
-    console.log(data);
     
     box.innerHTML = data?.map((item) => `
-    <div class="border border-spacing-x-96 bg-slate-800 p-3 mb-5 w-[250px]">
+    <div class="border rounded-[15px] bg-slate-800 p-6 mb-5 w-[250px]">
      <img src="${item.img}">
      <h2 class="font-bold text-lg text-green-700">${item.title}</h2>
      <p class="font-bold text-lg text-green-700">${item.rame}</p>
      <p class="font-bold text-lg text-green-700">${item.color}</p>
-     <h2 class="font-bold text-lg text-green-700">${item.brand}</h2>
+     <div class="flex justify-between mt-5">
+      <button class="border border-spacing-10 py-1 px-2 font-medium text-lg text-red-400 rounded-[10px]" data-name="${item.name}">${item.brand}</button>
      <h3 class="font-bold text-lg text-green-700">${item.price}</h3>
+     </div>
     </div>
     `).join("")
    
@@ -39,5 +52,24 @@ menu.addEventListener("click",(e)=>{
   
   if(e.target.dataset.name){
     renderData(e.target.dataset.name)
+  }
+})
+
+
+const renderBrand = async (brand) => {
+  const data = await catalogBrand(brand)
+
+  row.innerHTML = data?.map((item) => `
+     <div class="border rounded-[15px] bg-slate-800 p-6 mb-5 w-[250px]">
+     <img src="${item.img}">
+     <p class="font-bold text-lg text-green-700">${item.brand}</p>
+    </div>
+    `).join("")
+}
+
+row.addEventListener("click",(e)=>{
+  
+  if(e.target.dataset.name){
+    renderBrand(e.target.dataset.name)
   }
 })
